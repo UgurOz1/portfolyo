7c593ad5450b7c6155134d8f813a3d514bacef22
ugur <uguro9319@gmail.com>
2025-12-31 22:04:44 +0300

    pwa eklendi

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo.png'],
+      manifest: {
+        name: 'Uğur Özcan Portfolyo',
+        short_name: 'Uğur Özcan',
+        description: 'Uğur Özcan Kişisel Portfolyo Sitesi',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: process.env.NODE_ENV === 'production' ? '/portfolyo/' : '/',
   build: {
     outDir: 'dist',
